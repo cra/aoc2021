c0acbb0d932ae7133a3ca1de39ce982ae04c213d
Igor Mosyagin <igor.mosyagin@klarna.com>
2021-12-08 19:24:06 +0100

    upd day8 part 2

--- 08/script.py
+++ 08/script.py
@@ -1,4 +1,6 @@
-test = """
+import collections
+
+test: str = """
 be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
 edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
 fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
@@ -12,6 +14,25 @@ gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
 """
 
 
+TOP = 'a'
+TL = 'b'
+TR = 'c'
+MID = 'd'
+BL = 'e'
+BR = 'f'
+BOT = 'g'
+
+STAT10 = {
+    TOP: 8,
+    TL: 6,
+    TR: 8,
+    MID: 7,
+    BL: 4,
+    BR: 9,
+    BOT: 7,
+}
+
+
 def p1(entries):
     number_easy_digits = 0
     for line in entries:
@@ -23,29 +44,77 @@ def p1(entries):
 
 
 def solve_one_entry(entry):
-    mappers = {ch: '' for ch in 'abcdefg'}
+    mappers = {ch: [] for ch in 'abcdefg'}
     lhs, rhs = entry.split(" | ")
-    # find ones
     lhs_tokens = lhs.split()
+
+    # find BL, the only that has 4 occurencies
+    cnt = collections.Counter(''.join(lhs_tokens))
+    for k, v in cnt.most_common():
+        if v == 4:
+            for kk in mappers:
+                if BL in mappers[kk]:
+                    mappers[kk].remove(BL)
+            mappers[k] = [BL]
+
+    # find number one
     for token in lhs_tokens:
         if len(token) == 2:
-            # this is the one
+            print('found 2-symbols token', token)
             for letter in token:
-                mappers[letter] = ['c', 'f']
+                mappers[letter] = [TR, BR]
+            lhs_tokens.remove(token)
+            break
+    # find number 7: the only letter with three segments
+    for token in lhs_tokens:
         if len(token) == 3:
+            print('found 3-symbols token', token)
             for letter in token:
-                ...
-                #if mappers[
+                if (TR in mappers[letter]) or (BR in mappers[letter]):  # that's a side one
+                    continue
+                mappers[letter] = [TOP]
+            lhs_tokens.remove(token)
+            break
+    # find number 4: the only letter with four segments
+    for token in lhs_tokens:
+        if len(token) == 4:
+            print('found 4-symbols token', token)
+            for letter in token:
+                if (TR in mappers[letter]) or (BR in mappers[letter]):  # that's a side one
+                    mappers[letter] = [TL, MID]
+            lhs_tokens.remove(token)
+            break
+    # find number 8: the only letter with eight segments
+    for token in lhs_tokens:
+        if len(token) == 8:
+            print('found 8-symbols token', token)
+            for letter in token:
+                if any(
+                    TOP in mappers[letter],
+                    TL in mappers[letter],
+                    TR in mappers[letter],
+                    MID in mappers[letter],
+                    BR in mappers[letter],
+                    BL in mappers[letter],
+                ):
+                    continue
+                mappers[letter] = [BOT]
+            lhs_tokens.remove(token)
+            break
+    # since lhs_tokens was modified we can't recalc now, or can we?
+    # bottom left is the only one that is present in 7 positions
 
-
+    print("Known mappers", mappers)
+    print("Left with tokens", lhs_tokens)
 
 
 def p2(entries):
-    ...
+    solve_one_entry(entries[0])
 
 
 if __name__ == '__main__':
     test = test.strip().splitlines()
     print("P1 test", p1(test))
-    puzzle_input = [line.strip() for line in open("./puzzle_input").readlines()]
-    print("P1 real", p1(puzzle_input))
+    #puzzle_input = [line.strip() for line in open("./puzzle_input").readlines()]
+    #print("P1 real", p1(puzzle_input))
+    print("P2 test", p2(test))

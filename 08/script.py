8cb44e6c553561d9099b0917a39e35fc6ddad0be
Igor Mosyagin <408698+cra@users.noreply.github.com>
2021-12-08 23:08:21 +0100

    Add day8 part2 solution

--- 08/script.py
+++ 08/script.py
@@ -1,4 +1,6 @@
+import itertools
 import collections
+import operator
 
 test: str = """
 be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
@@ -13,23 +15,19 @@ egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
 gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
 """
 
+TOP, TL, TR, MID, BL, BR, BOT = 'TOP', 'TL', 'TR', 'MID', 'BL', 'BR', 'BOT'
 
-TOP = 'a'
-TL = 'b'
-TR = 'c'
-MID = 'd'
-BL = 'e'
-BR = 'f'
-BOT = 'g'
-
-STAT10 = {
-    TOP: 8,
-    TL: 6,
-    TR: 8,
-    MID: 7,
-    BL: 4,
-    BR: 9,
-    BOT: 7,
+NUMS_TO_SEGMS = {
+    0: (TOP, TL, TR, BL, BR, BOT),
+    1: (TR, BR),
+    2: (TOP, TR, MID, BL, BOT),
+    3: (TOP, TR, MID, BR, BOT),
+    4: (TL, TR, MID, BR),
+    5: (TOP, TL, MID, BR, BOT),
+    6: (TOP, TL, MID, BL, BR, BOT),
+    7: (TOP, TR, BR),
+    8: (TOP, TL, TR, MID, BL, BR, BOT),
+    9: (TOP, TL, TR, MID, BR, BOT),
 }
 
 
@@ -43,78 +41,91 @@ def p1(entries):
     return number_easy_digits
 
 
-def solve_one_entry(entry):
-    mappers = {ch: [] for ch in 'abcdefg'}
-    lhs, rhs = entry.split(" | ")
+def find_mapping_for_one_entry(entry, verbose=False):
+    mappers = {ch: None for ch in 'abcdefg'}
+    lhs, _ = entry.split(" | ")
     lhs_tokens = lhs.split()
 
     # find BL, the only that has 4 occurencies
+    # fintd BR, the only that has 9 occurencies
     cnt = collections.Counter(''.join(lhs_tokens))
+    if verbose: print(cnt.most_common())
     for k, v in cnt.most_common():
         if v == 4:
-            for kk in mappers:
-                if BL in mappers[kk]:
-                    mappers[kk].remove(BL)
-            mappers[k] = [BL]
+            mappers[k] = BL
+        if v == 6:  # FUUUUUCK this makes everything MUCH simplier
+            mappers[k] = TL
+        if v == 9:
+            mappers[k] = BR
 
-    # find number one
-    for token in lhs_tokens:
-        if len(token) == 2:
-            print('found 2-symbols token', token)
-            for letter in token:
-                mappers[letter] = [TR, BR]
-            lhs_tokens.remove(token)
-            break
+    # find number 1
+    token = next(token for token in lhs_tokens if len(token) == 2)
+    if verbose: print('found 2-symbols token', token)
+    for letter in (letter for letter in token if mappers[letter] != BR):
+        mappers[letter] = TR
+    lhs_tokens.remove(token)
+
     # find number 7: the only letter with three segments
-    for token in lhs_tokens:
-        if len(token) == 3:
-            print('found 3-symbols token', token)
-            for letter in token:
-                if (TR in mappers[letter]) or (BR in mappers[letter]):  # that's a side one
-                    continue
-                mappers[letter] = [TOP]
-            lhs_tokens.remove(token)
-            break
+    token = next(token for token in lhs_tokens if len(token) == 3)
+    if verbose: print('found 3-symbols token', token)
+    for letter in token:
+        if any(mappers[letter] == seg for seg in (TR, BR)):  # that's a side one
+            continue
+        mappers[letter] = TOP
+    lhs_tokens.remove(token)
+
     # find number 4: the only letter with four segments
-    for token in lhs_tokens:
-        if len(token) == 4:
-            print('found 4-symbols token', token)
-            for letter in token:
-                if (TR in mappers[letter]) or (BR in mappers[letter]):  # that's a side one
-                    mappers[letter] = [TL, MID]
-            lhs_tokens.remove(token)
-            break
+    token = next(token for token in lhs_tokens if len(token) == 4)
+    if verbose: print('found 4-symbols token', token)
+    for letter in token:
+        if any(mappers[letter] == seg for seg in [TL, TR, BR]):  # known so far
+            continue
+        mappers[letter] = MID
+    lhs_tokens.remove(token)
+
     # find number 8: the only letter with eight segments
-    for token in lhs_tokens:
-        if len(token) == 8:
-            print('found 8-symbols token', token)
-            for letter in token:
-                if any(
-                    TOP in mappers[letter],
-                    TL in mappers[letter],
-                    TR in mappers[letter],
-                    MID in mappers[letter],
-                    BR in mappers[letter],
-                    BL in mappers[letter],
-                ):
-                    continue
-                mappers[letter] = [BOT]
-            lhs_tokens.remove(token)
-            break
-    # since lhs_tokens was modified we can't recalc now, or can we?
-    # bottom left is the only one that is present in 7 positions
+    token = next(token for token in lhs_tokens if len(token) == 7)
+    if verbose: print('found 7-symbols token', token)
+    mappers[next(seg for seg in token if mappers[seg] == None)] = BOT
+    lhs_tokens.remove(token)
 
-    print("Known mappers", mappers)
-    print("Left with tokens", lhs_tokens)
+    return mappers
 
 
-def p2(entries):
-    solve_one_entry(entries[0])
+def solve_one_entry(entry, verbose=False):
+    if verbose: print(entry)
+    mappers = find_mapping_for_one_entry(entry, verbose=verbose)
+    inverse_mappers = {v:k for k,v in mappers.items()}
+    if verbose: print("inverse mappers",inverse_mappers)
+
+    TRANSLATION = {}
+    for num, segments in NUMS_TO_SEGMS.items():
+        scrambled_letters = (inverse_mappers[segment] for segment in segments)
+        sorted_letters = ''.join(sorted(scrambled_letters))
+        TRANSLATION[sorted_letters] = num
+
+    if verbose: print("Found translation", TRANSLATION)
+
+    rhs_tokens = entry.split(" | ")[1].split()
+    if verbose: print("Here are rhs:", rhs_tokens)
+    reading = ''
+    for token in rhs_tokens:
+        ordered_token  = ''.join(sorted(token))
+        num = str(TRANSLATION[ordered_token])
+        if verbose: print(f'{token} -> {num}')
+        reading += num
+
+    return int(reading)
+
+
+def p2(entries, verbose=False):
+    return sum(map(solve_one_entry, entries, itertools.repeat(verbose)))
 
 
 if __name__ == '__main__':
     test = test.strip().splitlines()
     print("P1 test", p1(test))
-    #puzzle_input = [line.strip() for line in open("./puzzle_input").readlines()]
-    #print("P1 real", p1(puzzle_input))
+    puzzle_input = [line.strip() for line in open("./puzzle_input").readlines()]
+    print("P1 real", p1(puzzle_input))
     print("P2 test", p2(test))
+    print("P2 real", p2(puzzle_input))
